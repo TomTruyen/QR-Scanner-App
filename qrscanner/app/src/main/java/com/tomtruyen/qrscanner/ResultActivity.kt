2f195181d70df5ee34017bf41f093e24d65abec7
TomTruyen <tom.truyen@gmail.com>
2021-07-18 14:28:41 +0200

    Add bannerAd

--- qrscanner/app/src/main/java/com/tomtruyen/qrscanner/ResultActivity.kt
+++ qrscanner/app/src/main/java/com/tomtruyen/qrscanner/ResultActivity.kt
@@ -2,21 +2,30 @@ package com.tomtruyen.qrscanner
 
 import android.content.Intent
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import java.lang.Exception
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var mAdView: AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
         supportActionBar?.title = "Result"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        MobileAds.initialize(this ) { }
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
 
         val url = intent.getStringExtra("URL")
 
