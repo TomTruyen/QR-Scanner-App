6a4dc7030f48328e23be8b1b78068abcda9f3889
TomTruyen <tom.truyen@gmail.com>
2021-07-20 13:29:37 +0200

    Fix closing scanner soft locking app

--- qrscanner/app/src/main/java/com/tomtruyen/qrscanner/MainActivity.kt
+++ qrscanner/app/src/main/java/com/tomtruyen/qrscanner/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 import com.google.zxing.integration.android.IntentIntegrator
 import com.journeyapps.barcodescanner.CaptureActivity
@@ -13,6 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        findViewById<Button>(R.id.startScannerButton).setOnClickListener {
+            startScanner()
+        }
+
+        startScanner()
+    }
+
+    private fun startScanner() {
         val scanner = IntentIntegrator(this)
         scanner.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
         scanner.setOrientationLocked(true)
